f44ec45e4404fcb8a8e983b9db33c5d2948b97ac
log1-codes <singhanurag1309@gmail.com>
2026-07-26 10:36:34 +0530

    replaced contact freeform credentials with Jay Gupta

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,7 +21,7 @@ export default function ContactPage() {
     const formData = new FormData(e.currentTarget);
 
     try {
-      const response = await fetch("https://formspree.io/f/xgogppea", {
+      const response = await fetch("https://formspree.io/f/xpqvoeqg", {
         method: "POST",
         body: formData,
         headers: {
